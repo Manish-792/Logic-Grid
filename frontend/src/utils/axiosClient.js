bc0e6e8e47863f791b7d45ba38a10c2ecfd3cf6e
Manish <manish.s4573@gmail.com>
2025-08-17 18:19:09 +0530

    fixing backend

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,15 +1,22 @@
-import axios from "axios";
+import axios from "axios"
+
+
 
 const axiosClient = axios.create({
-    // THIS IS THE FIX. We are removing the environment variable 
-    // and using a relative path.
-    baseURL: '/api', 
-    
-    withCredentials: true,
-    timeout: 5000, // 5 second timeout
-    headers: {
-        'Content-Type': 'application/json'
-    }
+
+baseURL: `${import.meta.env.VITE_PUBLIC_BUILDER_KEY}/api`,
+
+ withCredentials: true,
+
+ timeout: 5000, // 5 second timeout
+
+headers: {
+
+'Content-Type': 'application/json'
+
+}
+
 });
+
 
 export default axiosClient;
